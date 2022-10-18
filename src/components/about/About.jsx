a0a34fb41e2ea9575d50dea0bf04227c244c1c74
Abhishek5131 <abhishek5131singh@gmail.com>
2022-10-18 20:45:30 +0530

    added container in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+
 import './about.css'
+import Me from '../../assets/about-me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscRootFolder} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div id='about'>About</div>
+    <div id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="Wannabe Me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Year</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>Nill</small>
+            </article><article className='about__card'>
+              <VscRootFolder className='about__icon'/>
+              <h5>Projects</h5>
+              <small>2+ Completed Projects </small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
