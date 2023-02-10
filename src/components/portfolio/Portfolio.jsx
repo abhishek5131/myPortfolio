b815cf04b6c5ba365b0d3123205bee5a687a630e
abhishek5131 <abhishek5131singh@gmail.com>
2023-02-10 13:03:20 +0530

    test commit after months

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -45,8 +45,8 @@ const portfolio = () => {
           </div>
           <h3>{title}</h3>
           <div className="portfolio__item-cta">
-            <a href={github} className='btn' target='_blank'>Github</a>
-            <a href={demo} className='btn btn-primary' target='_blank'>Abhishek Live</a>
+            <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Abhishek Live</a>
           </div>
         </article>
             )
