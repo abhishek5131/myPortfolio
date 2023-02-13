e7e820bd3dbdf7fef5afe4a32da6ff302a17a9ac
abhishek5131 <abhishek5131singh@gmail.com>
2023-02-13 20:06:05 +0530

    updated file contents

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
 import './testimonials.css'
+import avtr1 from '../../assets/avatar1.jpg'
+import avtr2 from '../../assets/avatar2.jpg'
+import avtr3 from '../../assets/avatar3.jpg'
+import avtr4 from '../../assets/avatar4.jpg'
+
+
+const data = [
+  {
+    avatar: avtr1,
+    name: 'Nikhil Srivastav',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ad quae aperiam amet earum, provident laboriosam deleniti natus, perferendis fugit numquam repellat in ipsa voluptas dolores officia nihil. Impedit, fuga!'
+  },
+  {
+    avatar: avtr2,
+    name: 'Dheeraj Kanwal',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ad quae aperiam amet earum, provident laboriosam deleniti natus, perferendis fugit numquam repellat in ipsa voluptas dolores officia nihil. Impedit, fuga!'
+  },
+  {
+    avatar: avtr3,
+    name: 'Gaurav Belwal',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ad quae aperiam amet earum, provident laboriosam deleniti natus, perferendis fugit numquam repellat in ipsa voluptas dolores officia nihil. Impedit, fuga!'
+  },
+  {
+    avatar: avtr4,
+    name: 'I.D. Paliwal',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ad quae aperiam amet earum, provident laboriosam deleniti natus, perferendis fugit numquam repellat in ipsa voluptas dolores officia nihil. Impedit, fuga!'
+  }
+]
+
+
 const testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review, index}) => {
+            return(
+              <article key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} alt = 'reviewers'/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
