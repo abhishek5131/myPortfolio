d8c3693f6116f40fd996228158e36527923ac784
abhishek5131 <abhishek5131singh@gmail.com>
2023-02-16 17:07:37 +0530

    footer file updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+
+
 const footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <section id='footer'>
+      <a href="#" className='footer-logo'>ABS</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://twitter.com"><AiOutlineTwitter/></a>
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+        <a href="https://github.com"><BsGithub/></a>
+      </div>
+      <div className="footer__copyright">
+        <small> &copy; ABS. All Rights Reserved</small>
+      </div>
+    </section>
   )
 }
 
