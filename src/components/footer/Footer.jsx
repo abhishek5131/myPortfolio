7680b6f00cbff5075d0c77a532de0d0b2d2c36db
abhishek5131 <abhishek5131singh@gmail.com>
2023-02-16 18:01:31 +0530

    added value to line 7 & 13 of footer.jsx file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,10 +8,10 @@ import {BsGithub} from 'react-icons/bs'
 const footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>ABS</a>
+      <a href="#home" className='footer__logo'>ABS</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
