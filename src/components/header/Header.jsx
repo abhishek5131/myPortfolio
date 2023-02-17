73980ef77abb52fd64575fbd5eaecc236904db0a
abhishek5131 <abhishek5131singh@gmail.com>
2023-02-17 12:34:42 +0530

    updated header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const header = () => {
     <header id='home'>
       <div className='container header__container'>
         <h5>Hello I'm</h5>
-        <h1>Vijay Dangwal</h1>
+        <h1>Abhsihek Singh</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocials/>
